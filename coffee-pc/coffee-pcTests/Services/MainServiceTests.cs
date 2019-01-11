9e942026e3c7024d13743c8d847239c3dcd1b9fd
Gyulavári Peti <e137projects@gmail.com>
2019-01-11 19:49:57 +0100

    test fix url

--- coffee-pc/coffee-pcTests/Services/MainServiceTests.cs
+++ coffee-pc/coffee-pcTests/Services/MainServiceTests.cs
@@ -14,7 +14,8 @@ namespace coffee_pc.Service.Tests
     [TestClass()]
     public class MainServiceTests
     {
-        const String BASE_URL = "http://localhost:5819";
+        const String RES_URL = "http://localhost:5819";
+        const String AUTH_URL = "http://localhost:5821";
         MainService ms = new MainService();
 
         [TestMethod()]
@@ -26,7 +27,7 @@ namespace coffee_pc.Service.Tests
 
                 await ms.RequestLoginAsync("valami@mail.com", "asdasd123");
 
-                httpTest.ShouldHaveCalled($"{BASE_URL}/oauth2/token")
+                httpTest.ShouldHaveCalled($"{AUTH_URL}/oauth2/token")
                 .WithVerb(HttpMethod.Post)
                 .WithContentType("application/x-www-form-urlencoded")
                 .Times(1);
@@ -42,7 +43,7 @@ namespace coffee_pc.Service.Tests
 
                 await ms.RequestRegisterAsync("valami@mail.com", "asdasd123", "asdasd123");
 
-                httpTest.ShouldHaveCalled($"{BASE_URL}/api/accounts/create")
+                httpTest.ShouldHaveCalled($"{RES_URL}/api/accounts/create")
                 .WithVerb(HttpMethod.Post)
                 .WithContentType("application/json")
                 .Times(1);
